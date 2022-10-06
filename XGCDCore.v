9eb0080da3b094bae8ff7d80ccb3a791f8033555
Gedeon Nyengele <thenextged@gmail.com>
2022-10-05 19:05:50 -0700

    AXI Intf Test

--- XGCDCore.v
+++ XGCDCore.v
@@ -66,6 +66,18 @@ module XGCDCore #(parameter  WIDTH = 32) (
     reg     [31:0]              r_PRDATA_reg;
     reg     [31:0]              r_PRDATA;
 
+    wire                        sram_ce_n;
+    wire [31:0]                 sram_addr;
+    wire [63:0]                 sram_wdata;
+    wire                        sram_we_n;
+    wire [7:0]                  sram_wbe_n;
+    wire [63:0]                 sram_rdata;
+
+    reg  [63:0]                 r_ARG_A[31:0];
+    reg  [63:0]                 r_ARG_B[31:0];
+    reg  [63:0]                 r_MASK;
+    reg  [63:0]                 r_MemTemp;
+    reg  [63:0]                 r_RDATA;
 
     //
     // APB
@@ -96,38 +108,90 @@ module XGCDCore #(parameter  WIDTH = 32) (
     // AXI
     //
 
-    assign unused   =   (| AWID)            |
-                        (| AWADDR)          |
-                        (| AWLEN)           |
-                        (| AWSIZE)          |
-                        (| AWBURST)         |
-                        (| AWLOCK)          |
-                        (| AWCACHE)         |
-                        (| AWPROT)          |
-                        (| AWVALID)         |
-                        (| BREADY)          |
-                        (| ARID)            |
-                        (| ARADDR)          |
-                        (| ARLEN)           |
-                        (| ARSIZE)          |
-                        (| ARBURST)         |
-                        (| ARLOCK)          |
-                        (| ARCACHE)         |
-                        (| ARPROT)          |
-                        (| ARVALID)         |
-                        (| RREADY);
+    AXItoSRAM u_axi_to_sram (
+        .ACLK                         (CLK),
+        .ARESETn                      (RESETn),
 
-    assign AWREADY      = 1'b1;
-    assign WREADY       = 1'b1;
-    assign BID          = 4'h0;
-    assign BRESP        = 2'b00;
-    assign BVALID       = 1'b0;
-    assign ARREADY      = 1'b1;
-    assign RID          = 4'h0;
-    assign RRESP        = 2'b00;
-    assign RLAST        = 1'b1;
-    assign RDATA        = {64{1'b0}};
-    assign RVALID       = 1'b0;
+        .S_AXI_AWID                   (AWID),
+        .S_AXI_AWADDR                 (AWADDR),
+        .S_AXI_AWLEN                  (AWLEN),
+        .S_AXI_AWSIZE                 (AWSIZE),
+        .S_AXI_AWBURST                (AWBURST),
+        .S_AXI_AWLOCK                 (AWLOCK),
+        .S_AXI_AWCACHE                (AWCACHE),
+        .S_AXI_AWPROT                 (AWPROT),
+        .S_AXI_AWVALID                (AWVALID),
+        .S_AXI_AWREADY                (AWREADY),
+        .S_AXI_WDATA                  (WDATA),
+        .S_AXI_WSTRB                  (WSTRB),
+        .S_AXI_WLAST                  (WLAST),
+        .S_AXI_WVALID                 (WVALID),
+        .S_AXI_WREADY                 (WREADY),
+        .S_AXI_BID                    (BID),
+        .S_AXI_BRESP                  (BRESP),
+        .S_AXI_BVALID                 (BVALID),
+        .S_AXI_BREADY                 (BREADY),
+        .S_AXI_ARID                   (ARID),
+        .S_AXI_ARADDR                 (ARADDR),
+        .S_AXI_ARLEN                  (ARLEN),
+        .S_AXI_ARSIZE                 (ARSIZE),
+        .S_AXI_ARBURST                (ARBURST),
+        .S_AXI_ARLOCK                 (ARLOCK),
+        .S_AXI_ARCACHE                (ARCACHE),
+        .S_AXI_ARPROT                 (ARPROT),
+        .S_AXI_ARVALID                (ARVALID),
+        .S_AXI_ARREADY                (ARREADY),
+        .S_AXI_RID                    (RID),
+        .S_AXI_RDATA                  (RDATA),
+        .S_AXI_RRESP                  (RRESP),
+        .S_AXI_RLAST                  (RLAST),
+        .S_AXI_RVALID                 (RVALID),
+        .S_AXI_RREADY                 (RREADY),
+
+        .SRAM_CEn                     (sram_ce_n),
+        .SRAM_ADDR                    (sram_addr),
+        .SRAM_WDATA                   (sram_wdata),
+        .SRAM_WEn                     (sram_we_n),
+        .SRAM_WBEn                    (sram_wbe_n),
+        .SRAM_RDATA                   (sram_rdata)
+    );
+
+    // Write
+
+    always @(posedge CLK) begin
+        // ARG_A
+        if (~sram_ce_n & ~sram_we_n && (sram_addr[11:8] == 4'd0)) begin
+            r_MemTemp   = r_ARG_A[sram_addr[7:3]];
+            r_MASK      = {64{1'b0}};
+            for (i = 0; i < 8; i=i+1)
+                if (~sram_wbe_n[i]) r_MASK[i*8 +: 8] = {8{1'b1}};
+            r_ARG_A[sram_addr[7:3]] = (r_MemTemp & ~r_MASK) | (sram_wdata & r_MASK);
+        end
+        // ARG_B
+        if (~sram_ce_n & ~sram_we_n && (sram_addr[11:8] == 4'd1)) begin
+            r_MemTemp   = r_ARG_B[sram_addr[7:3]];
+            r_MASK      = {64{1'b0}};
+            for (i = 0; i < 8; i=i+1)
+                if (~sram_wbe_n[i]) r_MASK[i*8 +: 8] = {8{1'b1}};
+            r_ARG_B[sram_addr[7:3]] = (r_MemTemp & ~r_MASK) | (sram_wdata & r_MASK);
+        end
+    end
+
+    // Read
+
+    always @(posedge CLK or negedge RESETn)
+        if (!RESETn)
+            r_RDATA     <= {64{1'b0}};
+        else if (~sram_ce_n & sram_we_n)
+            case (sram_addr[11:8)
+                4'd0    : r_RDATA <= r_ARG_A[sram_addr[7:3]];
+                4'd1    : r_RDATA <= r_ARG_B[sram_addr[7:3]];
+                default : r_RDATA <= {64{1'b0}};
+            endcase
+        end
+
+    assign sram_rdata = r_RDATA;
+
 
     //
     // IRQ, START_OUT, DONE_OUT
