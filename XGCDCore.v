bbc2f9634afad2f0527d411866ad223019f8340b
Gedeon Nyengele <thenextged@gmail.com>
2022-10-05 18:06:40 -0700

    added XGCD core

---
+++ XGCDCore.v
@@ -0,0 +1,140 @@
+module XGCDCore #(parameter  WIDTH = 32) (
+    input       wire            CLK,
+    input       wire            RESETn,
+
+    input       wire [31:0]     PADDR,
+    input       wire            PSEL,
+    input       wire            PENABLE,
+    input       wire            PWRITE,
+    input       wire [31:0]     PWDATA,
+    output      wire [31:0]     PRDATA,
+    output      wire            PREADY,
+    output      wire            PSLVERR,
+
+    input       wire [3:0]      AWID,
+    input       wire [31:0]     AWADDR,
+    input       wire [7:0]      AWLEN,
+    input       wire [2:0]      AWSIZE,
+    input       wire [1:0]      AWBURST,
+    input       wire            AWLOCK,
+    input       wire [3:0]      AWCACHE,
+    input       wire [2:0]      AWPROT,
+    input       wire            AWVALID,
+    output      wire            AWREADY,
+    input       wire [63:0]     WDATA,
+    input       wire [7:0]      WSTRB,
+    input       wire            WLAST,
+    input       wire            WVALID,
+    output      wire            WREADY,
+    output      wire [3:0]      BID,
+    output      wire [1:0]      BRESP,
+    output      wire            BVALID,
+    input       wire            BREADY,
+    input       wire [3:0]      ARID,
+    input       wire [31:0]     ARADDR,
+    input       wire [7:0]      ARLEN,
+    input       wire [2:0]      ARSIZE,
+    input       wire [1:0]      ARBURST,
+    input       wire            ARLOCK,
+    input       wire [3:0]      ARCACHE,
+    input       wire [2:0]      ARPROT,
+    input       wire            ARVALID,
+    output      wire            ARREADY,
+    output      wire [3:0]      RID,
+    output      wire [63:0]     RDATA,
+    output      wire [1:0]      RRESP,
+    output      wire            RLAST,
+    output      wire            RVALID,
+    input       wire            RREADY,
+
+    output      wire            IRQ,
+    output      wire            START_OUT,
+    output      wire            DONE_OUT
+);
+
+    //
+    // Internal Signals
+    //
+
+    wire                        unused;
+
+    wire                        apb_setup;
+    wire                        apb_rd_en;
+    wire                        apb_wr_en;
+    wire    [9:0]               apb_addr_oft;
+
+    reg     [31:0]              r_PRDATA_reg;
+    reg     [31:0]              r_PRDATA;
+
+
+    //
+    // APB
+    //
+
+    assign apb_addr_oft         = PADDR[11:2];
+    assign apb_setup            = PSEL & ~PENABLE;
+    assign apb_rd_en            = apb_setup & ~PWRITE;
+    assign apb_wr_en            = apb_setup & PWRITE;
+
+    always @(*)
+        case (PADDR[11:2])
+            10'd0   : r_PRDATA_reg  = 32'h5A5A5A5A;
+            default : r_PRDATA_reg  = {32{1'b0}};
+        endcase
+
+    always @(posedge CLK or negedge RESETn)
+        if (!RESETn)
+            r_PRDATA    <= {32{1'b0}};
+        else if (apb_rd_en)
+            r_PRDATA    <= r_PRDATA_reg;
+
+    assign PREADY       = 1'b1;
+    assign PSLVERR      = 1'b0;
+    assign PRDATA       = r_PRDATA;
+
+    //
+    // AXI
+    //
+
+    assign unused   =   (| AWID)            |
+                        (| AWADDR)          |
+                        (| AWLEN)           |
+                        (| AWSIZE)          |
+                        (| AWBURST)         |
+                        (| AWLOCK)          |
+                        (| AWCACHE)         |
+                        (| AWPROT)          |
+                        (| AWVALID)         |
+                        (| BREADY)          |
+                        (| ARID)            |
+                        (| ARADDR)          |
+                        (| ARLEN)           |
+                        (| ARSIZE)          |
+                        (| ARBURST)         |
+                        (| ARLOCK)          |
+                        (| ARCACHE)         |
+                        (| ARPROT)          |
+                        (| ARVALID)         |
+                        (| RREADY);
+
+    assign AWREADY      = 1'b1;
+    assign WREADY       = 1'b1;
+    assign BID          = 4'h0;
+    assign BRESP        = 2'b00;
+    assign BVALID       = 1'b1;
+    assign ARREADY      = 1'b1;
+    assign RID          = 4'h0;
+    assign RRESP        = 2'b00;
+    assign RLAST        = 1'b1;
+    assign RDATA        = {64{1'b0}};
+    assign RVALID       = 1'b1;
+
+    //
+    // IRQ, START_OUT, DONE_OUT
+    //
+
+    assign IRQ          = 1'b0;
+    assign START_OUT    = 1'b0;
+    assign DONE_OUT     = 1'b0;
+
+endmodule
--- XGCDWrapperTop.v
+++ XGCDWrapperTop.v
@@ -123,149 +123,160 @@ module XGCDWrapperTop (
     input       wire            S_AXI_RREADY_1279
 );
 
-    // Prevent Linting Messages
-    wire unused =  (|S_APB_PADDR_255)      |
-                    S_APB_PSEL_255          |
-                    S_APB_PENABLE_255       |
-                    S_APB_PWRITE_255        |
-                    (|S_APB_PWDATA_255)     |
-                    (|S_APB_PADDR_1279)     |
-                    S_APB_PSEL_1279         |
-                    S_APB_PENABLE_1279      |
-                    S_APB_PWRITE_1279       |
-                    (|S_APB_PWDATA_1279)    |
-                    (|S_AXI_AWID_255)       |
-                    (|S_AXI_AWADDR_255)     |
-                    (|S_AXI_AWLEN_255)      |
-                    (|S_AXI_AWSIZE_255)     |
-                    (|S_AXI_AWBURST_255)    |
-                    S_AXI_AWLOCK_255        |
-                    (|S_AXI_AWCACHE_255)    |
-                    (|S_AXI_AWPROT_255)     |
-                    S_AXI_AWVALID_255       |
-                    (|S_AXI_WDATA_255)      |
-                    (|S_AXI_WSTRB_255)      |
-                    S_AXI_WLAST_255         |
-                    S_AXI_WVALID_255        |
-                    S_AXI_BREADY_255        |
-                    (|S_AXI_ARID_255)       |
-                    (|S_AXI_ARADDR_255)     |
-                    (|S_AXI_ARLEN_255)      |
-                    (|S_AXI_ARSIZE_255)     |
-                    (|S_AXI_ARBURST_255)    |
-                    S_AXI_ARLOCK_255        |
-                    (|S_AXI_ARCACHE_255)    |
-                    (|S_AXI_ARPROT_255)     |
-                    S_AXI_ARVALID_255       |
-                    S_AXI_RREADY_255        |
-                    (|S_AXI_AWID_1279)      |
-                    (|S_AXI_AWADDR_1279)    |
-                    (|S_AXI_AWLEN_1279)     |
-                    (|S_AXI_AWSIZE_1279)    |
-                    (|S_AXI_AWBURST_1279)   |
-                    S_AXI_AWLOCK_1279       |
-                    (|S_AXI_AWCACHE_1279)   |
-                    (|S_AXI_AWPROT_1279)    |
-                    S_AXI_AWVALID_1279      |
-                    (|S_AXI_WDATA_1279)     |
-                    (|S_AXI_WSTRB_1279)     |
-                    S_AXI_WLAST_1279        |
-                    S_AXI_WVALID_1279       |
-                    S_AXI_BREADY_1279       |
-                    (|S_AXI_ARID_1279)      |
-                    (|S_AXI_ARADDR_1279)    |
-                    (|S_AXI_ARLEN_1279)     |
-                    (|S_AXI_ARSIZE_1279)    |
-                    (|S_AXI_ARBURST_1279)   |
-                    S_AXI_ARLOCK_1279       |
-                    (|S_AXI_ARCACHE_1279)   |
-                    (|S_AXI_ARPROT_1279)    |
-                    S_AXI_ARVALID_1279      |
-                    S_AXI_RREADY_1279       ;
-
     //
     // Internal Signals
     //
 
-    wire            CLK;
-    wire            RESETn;
+    wire                        CLK;
+    wire                        RESETn;
 
     //
     // Clock And Reset Module
     //
 
     ClockAndReset u_clk_reset (
-        .EXT_CLK    (clk_in_extern),
-        .SYS_CLK    (clk_in_system),
-        .CLK_SEL    (clk_select),
-        .PORESETn   (reset_n),
-        .CLK        (CLK),
-        .RESETn     (RESETn)
+        .EXT_CLK                (clk_in_extern),
+        .SYS_CLK                (clk_in_system),
+        .CLK_SEL                (clk_select),
+        .PORESETn               (reset_n),
+        .CLK                    (CLK),
+        .RESETn                 (RESETn)
     );
 
-    assign clk_div_8 = CLK;
+    assign clk_div_8            = CLK;
 
     //
     // RO APB
     //
 
     RO u_ro_apb (
-        .CLK        (CLK),
-        .RESETn     (RESETn),
+        .CLK                    (CLK),
+        .RESETn                 (RESETn),
 
-        .PADDR      (S_APB_PADDR_RO),
-        .PSEL       (S_APB_PSEL_RO),
-        .PENABLE    (S_APB_PENABLE_RO),
-        .PWRITE     (S_APB_PWRITE_RO),
-        .PWDATA     (S_APB_PWDATA_RO),
-        .PRDATA     (S_APB_PRDATA_RO),
-        .PREADY     (S_APB_PREADY_RO),
-        .PSLVERR    (S_APB_PSLVERR_RO)
+        .PADDR                  (S_APB_PADDR_RO),
+        .PSEL                   (S_APB_PSEL_RO),
+        .PENABLE                (S_APB_PENABLE_RO),
+        .PWRITE                 (S_APB_PWRITE_RO),
+        .PWDATA                 (S_APB_PWDATA_RO),
+        .PRDATA                 (S_APB_PRDATA_RO),
+        .PREADY                 (S_APB_PREADY_RO),
+        .PSLVERR                (S_APB_PSLVERR_RO)
     );
 
     //
-    // Output Assignments
+    // XGCD_255
     //
 
+    XGCDCore #(.WIDTH(255)) u_xgcd_255 (
+        .CLK                    (CLK),
+        .RESETn                 (RESETn),
 
-    assign start_out_255        = 1'b0;
-    assign start_out_1279       = 1'b0;
-    assign done_out_255         = 1'b0;
-    assign done_out_1279        = 1'b0;
+        .PADDR                  (S_APB_PADDR_255),
+        .PSEL                   (S_APB_PSEL_255),
+        .PENABLE                (S_APB_PENABLE_255),
+        .PWRITE                 (S_APB_PWRITE_255),
+        .PWDATA                 (S_APB_PWDATA_255),
+        .PRDATA                 (S_APB_PRDATA_255),
+        .PREADY                 (S_APB_PREADY_255),
+        .PSLVERR                (S_APB_PSLVERR_255),
 
-    assign IRQ_255              = 1'b0;
-    assign IRQ_1279             = 1'b0;
+        .AWID                   (S_AXI_AWID_255),
+        .AWADDR                 (S_AXI_AWADDR_255),
+        .AWLEN                  (S_AXI_AWLEN_255),
+        .AWSIZE                 (S_AXI_AWSIZE_255),
+        .AWBURST                (S_AXI_AWBURST_255),
+        .AWLOCK                 (S_AXI_AWLOCK_255),
+        .AWCACHE                (S_AXI_AWCACHE_255),
+        .AWPROT                 (S_AXI_AWPROT_255),
+        .AWVALID                (S_AXI_AWVALID_255),
+        .AWREADY                (S_AXI_AWREADY_255),
+        .WDATA                  (S_AXI_WDATA_255),
+        .WSTRB                  (S_AXI_WSTRB_255),
+        .WLAST                  (S_AXI_WLAST_255),
+        .WVALID                 (S_AXI_WVALID_255),
+        .WREADY                 (S_AXI_WREADY_255),
+        .BID                    (S_AXI_BID_255),
+        .BRESP                  (S_AXI_BRESP_255),
+        .BVALID                 (S_AXI_BVALID_255),
+        .BREADY                 (S_AXI_BREADY_255),
+        .ARID                   (S_AXI_ARID_255),
+        .ARADDR                 (S_AXI_ARADDR_255),
+        .ARLEN                  (S_AXI_ARLEN_255),
+        .ARSIZE                 (S_AXI_ARSIZE_255),
+        .ARBURST                (S_AXI_ARBURST_255),
+        .ARLOCK                 (S_AXI_ARLOCK_255),
+        .ARCACHE                (S_AXI_ARCACHE_255),
+        .ARPROT                 (S_AXI_ARPROT_255),
+        .ARVALID                (S_AXI_ARVALID_255),
+        .ARREADY                (S_AXI_ARREADY_255),
+        .RID                    (S_AXI_RID_255),
+        .RDATA                  (S_AXI_RDATA_255),
+        .RRESP                  (S_AXI_RRESP_255),
+        .RLAST                  (S_AXI_RLAST_255),
+        .RVALID                 (S_AXI_RVALID_255),
+        .RREADY                 (S_AXI_RREADY_255),
 
-    assign S_APB_PRDATA_255     = {32{1'b0}};
-    assign S_APB_PREADY_255     = 1'b1;
-    assign S_APB_PSLVERR_255    = 1'b0;
+        .IRQ                    (IRQ_255),
+        .START_OUT              (start_out_255),
+        .DONE_OUT               (done_out_255)
+    );
 
-    assign S_APB_PRDATA_1279    = {32{1'b0}};
-    assign S_APB_PREADY_1279    = 1'b1;
-    assign S_APB_PSLVERR_1279   = 1'b0;
+    //
+    // XGCD_1279
+    //
 
-    assign S_AXI_AWREADY_255    = 1'b1;
-    assign S_AXI_WREADY_255     = 1'b1;
-    assign S_AXI_BID_255        = 4'h0;
-    assign S_AXI_BRESP_255      = 2'b00;
-    assign S_AXI_BVALID_255     = 1'b0;
-    assign S_AXI_ARREADY_255    = 1'b1;
-    assign S_AXI_RID_255        = 4'h0;
-    assign S_AXI_RDATA_255      = {64{1'b0}};
-    assign S_AXI_RRESP_255      = 2'b00;
-    assign S_AXI_RLAST_255      = 1'b0;
-    assign S_AXI_RVALID_255     = 1'b0;
+    XGCDCore #(.WIDTH(1279)) u_xgcd_1279 (
+        .CLK                    (CLK),
+        .RESETn                 (RESETn),
 
-    assign S_AXI_AWREADY_1279   = 1'b1;
-    assign S_AXI_WREADY_1279    = 1'b1;
-    assign S_AXI_BID_1279       = 4'h0;
-    assign S_AXI_BRESP_1279     = 2'b00;
-    assign S_AXI_BVALID_1279    = 1'b0;
-    assign S_AXI_ARREADY_1279   = 1'b1;
-    assign S_AXI_RID_1279       = 4'h0;
-    assign S_AXI_RDATA_1279     = {64{1'b0}};
-    assign S_AXI_RRESP_1279     = 2'b00;
-    assign S_AXI_RLAST_1279     = 1'b0;
-    assign S_AXI_RVALID_1279    = 1'b0;
+        .PADDR                  (S_APB_PADDR_1279),
+        .PSEL                   (S_APB_PSEL_1279),
+        .PENABLE                (S_APB_PENABLE_1279),
+        .PWRITE                 (S_APB_PWRITE_1279),
+        .PWDATA                 (S_APB_PWDATA_1279),
+        .PRDATA                 (S_APB_PRDATA_1279),
+        .PREADY                 (S_APB_PREADY_1279),
+        .PSLVERR                (S_APB_PSLVERR_1279),
+
+        .AWID                   (S_AXI_AWID_1279),
+        .AWADDR                 (S_AXI_AWADDR_1279),
+        .AWLEN                  (S_AXI_AWLEN_1279),
+        .AWSIZE                 (S_AXI_AWSIZE_1279),
+        .AWBURST                (S_AXI_AWBURST_1279),
+        .AWLOCK                 (S_AXI_AWLOCK_1279),
+        .AWCACHE                (S_AXI_AWCACHE_1279),
+        .AWPROT                 (S_AXI_AWPROT_1279),
+        .AWVALID                (S_AXI_AWVALID_1279),
+        .AWREADY                (S_AXI_AWREADY_1279),
+        .WDATA                  (S_AXI_WDATA_1279),
+        .WSTRB                  (S_AXI_WSTRB_1279),
+        .WLAST                  (S_AXI_WLAST_1279),
+        .WVALID                 (S_AXI_WVALID_1279),
+        .WREADY                 (S_AXI_WREADY_1279),
+        .BID                    (S_AXI_BID_1279),
+        .BRESP                  (S_AXI_BRESP_1279),
+        .BVALID                 (S_AXI_BVALID_1279),
+        .BREADY                 (S_AXI_BREADY_1279),
+        .ARID                   (S_AXI_ARID_1279),
+        .ARADDR                 (S_AXI_ARADDR_1279),
+        .ARLEN                  (S_AXI_ARLEN_1279),
+        .ARSIZE                 (S_AXI_ARSIZE_1279),
+        .ARBURST                (S_AXI_ARBURST_1279),
+        .ARLOCK                 (S_AXI_ARLOCK_1279),
+        .ARCACHE                (S_AXI_ARCACHE_1279),
+        .ARPROT                 (S_AXI_ARPROT_1279),
+        .ARVALID                (S_AXI_ARVALID_1279),
+        .ARREADY                (S_AXI_ARREADY_1279),
+        .RID                    (S_AXI_RID_1279),
+        .RDATA                  (S_AXI_RDATA_1279),
+        .RRESP                  (S_AXI_RRESP_1279),
+        .RLAST                  (S_AXI_RLAST_1279),
+        .RVALID                 (S_AXI_RVALID_1279),
+        .RREADY                 (S_AXI_RREADY_1279),
+
+        .IRQ                    (IRQ_1279),
+        .START_OUT              (start_out_1279),
+        .DONE_OUT               (done_out_1279)
+    );
 
 endmodule
